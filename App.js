c28dad1d44a11ef312497a578f5b74fd40203bb3
Breno-V <brenosuvale@gmail.com>
2026-08-23 18:39:26 -0300

    feat - integrar navegação stack com tela inicial e permissões

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ImagePickerComponent from './src/components/ImagePicker/ImagePickerComponent';
+import ContactsComponent from './src/components/Contacts/ContactsComponent';
+import LocationComponent from './src/components/Location/LocationComponent';
+import SensorComponent from './src/components/Sensors/SensorComponent';
+import RegistroVisitaScreen from './src/screens/RegistroVisitaScreen';
+import StartScreen from './src/screens/StartScreen';
+import HistoricoScreen from './src/screens/HistoricoScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Start" component={StartScreen} options={{ title: 'Início' }} />
+                <Stack.Screen name="ImagePicker" component={ImagePickerComponent} options={{ title: 'Galeria/Câmera' }} />
+                <Stack.Screen name="Contacts" component={ContactsComponent} options={{ title: 'Agenda' }} />
+                <Stack.Screen name="Location" component={LocationComponent} options={{ title: 'Localização' }} />
+                <Stack.Screen name="Sensors" component={SensorComponent} options={{ title: 'Sensores' }} />
+                <Stack.Screen name="RegistroVisita" component={RegistroVisitaScreen} options={{ title: 'Registro de Visita' }} />
+                <Stack.Screen name="Historico" component={HistoricoScreen} options={{ title: 'Histórico de Visitas' }} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
